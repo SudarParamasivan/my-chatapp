d500839d66b47ce256ffcfcc449670220d5d6fb4
SUDAR <118349021+SudarParamasivan@users.noreply.github.com>
2023-04-20 21:04:19 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { useState } from "react";
 import Chat from "./Chat";
 
 
-const socket = io.connect("https://chat-app-backend-sc0d.onrender.com");
+const socket = io.connect("https://my-chatapp-backend.onrender.com");
 
 function App() {
   const [username, setUsername] = useState("");
